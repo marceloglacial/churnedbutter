96ecc90f64c23757367bff2ee16ab85b6df3545c
Marcelo Feitas <mfreitas@postmedia.com>
2019-04-12 13:12:40 -0400

    Change files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 // ===================================================
-// 1. Setup
+// 1. Project Setup
 // ===================================================
 
 // 1.1 - Gulp modules
@@ -13,30 +13,48 @@ const gulp = require('gulp'),
     rename = require('gulp-rename');
 
 // 1.2 - Project Paths
-const root = './src/';
+const app = './src/';
 const dist = './dist/';
 const all = '**/*.*';
+const folders = '**/*';
 
-const src = new function () {
-    this.all = root + all;
-    this.root = root;
-    this.dist = dist;
-    this.styles = this.root + 'styles/**/*.scss';
-    this.assets = 'websites/assets/**/*.*';
-    this.templates = this.root + 'templates/';
-    this.partials = this.templates + 'partials/';
-    this.components = this.partials + 'components/**/*.*';
-}
 
 // ===================================================
-// 2. Styles Framweork
+// 2. General Functions
 // ===================================================
 
-// 2.1 - Compile SASS and minify CSS
+// 2.1 - Clean dist folder
 // ---------------------------------------------------
-function styles() {
+function clean() {
+    return del(dist);
+};
+exports.clean = clean;
+
+// 2.2 - Reload page
+// ---------------------------------------------------
+function liveReload() {
+    browserSync.reload();
+};
+
+
+// ===================================================
+// 3. Framework
+// ===================================================
+
+// 3.1 - Paths
+// ---------------------------------------------------
+const framework = new function () {
+    this.folder = app + 'framework/'
+    this.dist = dist + 'framework/';
+    this.styles = this.folder + '**/*.scss';
+    this.all = this.folder + all;
+};
+
+// 2.2 - Build (compile SASS)
+// ---------------------------------------------------
+function frameworkBuild() {
     return gulp
-        .src(src.styles)
+        .src(framework.styles)
         .pipe(sourcemaps.init())
         .pipe(sass({
             outputStyle: 'compressed'
@@ -46,106 +64,36 @@ function styles() {
             browsers: ['last 3 versions'],
         }))
         .pipe(sourcemaps.write('./maps'))
-        .pipe(gulp.dest(src.dist + 'css/'))
-    // .pipe(browserSync.stream());
+        .pipe(gulp.dest(framework.dist + 'css/'))
 };
-exports.styles = styles
+exports.frameworkBuild = frameworkBuild;
 
-// 2.2 - Handlebars templates
-// ---------------------------------------------------
-function templates() {
-    var templateData = {},
-        options = {
-            ignorePartials: true,
-            batch: [src.partials]
-        }
-    return gulp.src(src.templates + '*.hbs')
-        .pipe(handlebars(templateData, options))
-        .pipe(rename(function (path) {
-            path.extname = '.html';
-        }))
-        .pipe(gulp.dest(src.dist))
-    // .pipe(browserSync.stream());
-};
-exports.templates = templates;
-
-// 2.2 - Assets
-// ---------------------------------------------------
-function assets() {
-    return gulp.src(src.assets)
-        .pipe(gulp.dest(dist + '/assets/'))
-};
-exports.assets = assets;
-
-// 2.3 - Reload page
-// ---------------------------------------------------
-function liveReload() {
-    browserSync.reload();
-};
-
-// 2.4 - Start server and watch files
-// ---------------------------------------------------
-function liveServer() {
-    browserSync.init({
-        server: {
-            baseDir: dist
-        }
-    });
-    gulp.watch(src.all).on('change', gulp.series(build, liveReload));
-};
-
-// 2.5 - Build
-// ---------------------------------------------------
-function clean(path = dist) {
-    return del(path);
-};
-const cleanDist = gulp.series(() => clean());
-const build = gulp.series(cleanDist, templates, styles, assets);
 
 // ===================================================
-// 2. Styleguide
+// 3. Styleguide
 // ===================================================
 
-// 2.1 - Paths 
+// 3.1 - Paths 
 // ---------------------------------------------------
 const styleguide = new function () {
-    this.folder = 'styleguide/';
+    this.folder = app + 'styleguide/'
+    this.dist = dist + 'styleguide/';
+    this.assets = this.folder + 'assets/**/*.*';
+    this.styles = this.folder + 'styles/*.scss';
+    this.templates = this.folder + 'templates/*.hbs';
+    this.partials = this.folder + 'templates/partials';
     this.all = this.folder + all;
-    this.root = this.folder;
-    this.dist = this.folder + 'build/';
-    this.styles = this.root + 'styles/**/*.scss';
-    this.assets = this.root + 'assets/**/*.*';
-    this.templatesAll = this.root + 'templates/**/*.hbs';
-    this.templates = this.root + 'templates/*.hbs';
-    this.partials = this.root + 'templates/partials/';
-    this.components = this.root + 'templates/components/';
-}
-
-// 2.2 - Handlebars templates
-// ---------------------------------------------------
-function copyComponents() {
-    return gulp.src(src.components)
-        .pipe(gulp.dest(styleguide.components))
-}
-exports.copyComponents = copyComponents
-
-function styleguideTemplates() {
-    var templateData = {},
-        options = {
-            ignorePartials: true,
-            batch: [styleguide.partials]
-        }
-    return gulp.src(styleguide.templates)
-        .pipe(handlebars(templateData, options))
-        .pipe(rename(function (path) {
-            path.extname = '.html';
-        }))
-        .pipe(gulp.dest(styleguide.dist))
-    // .pipe(browserSync.stream());
 };
-exports.styleguideTemplates = styleguideTemplates;
 
-// 2.2 - SASS Files
+// 3.2 - Assets
+// ---------------------------------------------------
+function styleguideAssets() {
+    return gulp.src(styleguide.assets)
+        .pipe(gulp.dest(styleguide.dist + 'assets/'));
+};
+exports.styleguideAssets = styleguideAssets;
+
+// 3.3 - Styles
 // ---------------------------------------------------
 function styleguideStyles() {
     return gulp
@@ -160,24 +108,39 @@ function styleguideStyles() {
         }))
         .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest(styleguide.dist + 'assets/css/'))
-    // .pipe(browserSync.stream());
 };
 exports.styleguideStyles = styleguideStyles
 
 
-// 2.3 - Assets
+// 3.4 - Templates
 // ---------------------------------------------------
-function styleguideAssets() {
-    return gulp.src(styleguide.assets)
-        .pipe(gulp.dest(styleguide.dist + 'assets/'))
+function styleguideTemplates() {
+    var templateData = {},
+        options = {
+            ignorePartials: true,
+            batch: [styleguide.partials]
+        }
+    return gulp.src(styleguide.templates)
+        .pipe(handlebars(templateData, options))
+        .pipe(rename(function (path) {
+            path.extname = '.html';
+        }))
+        .pipe(gulp.dest(styleguide.dist))
 };
-exports.styleguideAssets = styleguideAssets;
+exports.styleguideTemplates = styleguideTemplates;
 
-// 2.4 - Cleand Build
-const styleguideClean = gulp.series(() => clean(styleguide.dist),() => clean(styleguide.components));
-gulp.task('styleguideClean', styleguideClean);
+// 3.5 - Build
+// ---------------------------------------------------
+gulp.task('styleguideBuild', 
+    gulp.series(
+        clean,
+        styleguideAssets,
+        styleguideStyles,
+        styleguideTemplates
+    )
+);
 
-// 2.5 - Live Server
+// 3.5 - Live Server
 // ---------------------------------------------------
 function styleguideLiveServer() {
     browserSync.init({
@@ -185,32 +148,19 @@ function styleguideLiveServer() {
             baseDir: styleguide.dist
         }
     });
-    gulp.watch(styleguide.styles).on('change', gulp.series(styleguideBuild, liveReload));
-    gulp.watch(styleguide.templates).on('change', gulp.series(styleguideBuild, liveReload));
-    gulp.watch(styleguide.partials + all).on('change', gulp.series(styleguideBuild, liveReload));
-    gulp.watch(styleguide.assets).on('change', gulp.series(styleguideBuild, liveReload));
+    gulp.watch(framework.all).on('change', gulp.series('frameworkBuild', liveReload));
+    gulp.watch(styleguide.all).on('change', gulp.series('styleguideBuild', liveReload));
 };
+exports.styleguideLiveServer = styleguideLiveServer;
 
-// 2.6 - Build
-// ---------------------------------------------------
-const styleguideBuild = gulp.series(
-    styleguideClean,
-    copyComponents,
-    styleguideAssets,
-    styleguideTemplates,
-    styleguideStyles
-);
 
 // ===================================================
-// 3. Gulp Main Tasks
+// 4. Demo / wwww
 // ===================================================
 
-gulp.task('clean', gulp.series(cleanDist, styleguideClean));
-gulp.task('develop', gulp.series(build, liveServer));
-gulp.task('build', build);
-gulp.task('default', gulp.series('develop'));
-gulp.task('styleguideBuild', styleguideBuild);
-gulp.task('styleguideStart', gulp.series(
-    styleguideBuild,
-    styleguideLiveServer
-));
+
+// ===================================================
+// 5. Gulp Tasks
+// ===================================================
+gulp.task('default', gulp.series(styleguideLiveServer));
+gulp.task('build', gulp.series(frameworkBuild));
